798976d5dc194a8bb3feb0801953065d9cb26b73
ys <bs45yh@yahoo.co.jp>
2017-12-17 00:13:14 +0900

    2.2.6

--- Contents/scripts/sisidebar/sisidebar_main.py
+++ Contents/scripts/sisidebar/sisidebar_main.py
@@ -51,7 +51,7 @@ else:
     image_path = os.path.join(os.path.dirname(__file__), 'icon/')
 #-------------------------------------------------------------
 pre_sel_group_but = False
-version = ' - SI Side Bar / ver_2.2.5 -'
+version = ' - SI Side Bar / ver_2.2.6 -'
 window_name = 'SiSideBar'
 window_width = 183
 top_hover = False#トップレベルボタンがホバーするかどうか
